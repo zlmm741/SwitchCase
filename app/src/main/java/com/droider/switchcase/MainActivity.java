76c2bfe69308453fae62f58ddce6105a7e9aea7f
Muxi <122283095@qq.com>
2020-03-11 17:30:48 +0800

    First commit

---
+++ app/src/main/java/com/droider/switchcase/MainActivity.java
@@ -0,0 +1,88 @@
+package com.droider.switchcase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String str1 = packedSwitch(1);
+        Toast.makeText(this,
+                str1,
+                Toast.LENGTH_SHORT).show();
+        String str2 = sparseSwitch(35);
+        Toast.makeText(this,
+                str2,
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+    private String packedSwitch(int i) {
+        String str = null;
+        switch (i) {
+            case 0: {
+                str = "She is a baby";
+                break;
+            }
+            case 1: {
+                str = "She is a girl";
+                break;
+            }
+            case 2: {
+                str = "She is a women";
+                break;
+            }
+            case 3: {
+                str = "She is a obasan";
+                break;
+            }
+            default: {
+                str = "She is a person";
+                break;
+            }
+        }
+        return str;
+    }
+
+    private String sparseSwitch(int age) {
+        String str = null;
+        switch (age) {
+            case 5: {
+                str = "He is a baby";
+                break;
+            }
+            case 15: {
+                str = "He is a student";
+                break;
+            }
+            case 35: {
+                str = "He is a father";
+                break;
+            }
+            case 65: {
+                str = "He is a grandpa";
+                break;
+            }
+            default: {
+                str = "He is a person";
+                break;
+            }
+        }
+        return str;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
